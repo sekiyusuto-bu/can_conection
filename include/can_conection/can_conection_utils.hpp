cabca0e287309c8d26cc711a77e3a698e5be72fd
shun <shundashiqi@gmail.com>
2025-02-15 12:06:20 +0900

    system change

--- include/can_conection/can_conection_utils.hpp
+++ include/can_conection/can_conection_utils.hpp
@@ -2,59 +2,15 @@
 #include <robomas_plugins/msg/frame.hpp>
 
 namespace can_conection::solenoid_utils{
-    inline auto to_high_mode() -> robomas_plugins::msg::Frame{
+    inline auto to_can_mode(uint32_t solenoid_id, uint8_t solenoid_data0, uint8_t solenoid_data1) -> robomas_plugins::msg::Frame{
     robomas_plugins::msg::Frame message{};
-    message.id = 0x250;
+    message.id = solenoid_id;
     message.is_rtr = false;
     message.is_extended = false;
     message.is_error = false;
     message.dlc = 8;
-    message.data[0] = 1;
-    return message;
-    }
-
-    inline auto to_low_mode() -> robomas_plugins::msg::Frame {
-    robomas_plugins::msg::Frame message{};
-    message.id = 0x250;
-    message.is_rtr = false;
-    message.is_extended = false;
-    message.is_error = false;
-    message.dlc = 8;
-    message.data[1] = 1;
-    return message;
-    }
-
-    inline auto to_air_mode() -> robomas_plugins::msg::Frame{
-    robomas_plugins::msg::Frame message{};
-    message.id = 0x251;
-    message.is_rtr = false;
-    message.is_extended = false;
-    message.is_error = false;
-    message.dlc = 8;
-    message.data[0] = 1;
-    return message;
-    }
-    
-    inline auto to_right_mode() -> robomas_plugins::msg::Frame {
-    robomas_plugins::msg::Frame message{};
-    message.id = 0x251;
-    message.is_rtr = false;
-    message.is_extended = false;
-    message.is_error = false;
-    message.dlc = 8;
-    message.data[1] = 1;
-    return message;
-    }
-
-    inline auto to_zero_mode() -> robomas_plugins::msg::Frame {
-    robomas_plugins::msg::Frame message{};
-    message.id = 0x251;
-    message.is_rtr = false;
-    message.is_extended = false;
-    message.is_error = false;
-    message.dlc = 8;
-    message.data[0] = 0;
-    message.data[1] = 0;
+    message.data[0] = solenoid_data0;
+    message.data[1] = solenoid_data1;
     return message;
     }
 }
